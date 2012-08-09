0c7fed63e686e3e6e7d6304a9b914d6ee65d14f6
Ariana Rizzitano <arizzitano@gmail.com>
2012-08-09 10:46:41 -0400

    added changes from before

--- silence.user.js
+++ silence.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name				Silence
-// @description	Hides comments.
+// @description	        Hides comments.
 
 // ==/UserScript==
 
@@ -8,29 +8,19 @@
     var safeDomains =   [
                             'jquery.com'
                         ],
-        containers =    [
-                            '#disqus_thread', 
-                            'fb:comments',
-                            '#echo_container_a', // washpo
-                            '.comments_block_holder', // huffpo
-                            '#ugccmt-container' // yahoo
-                        ],
+        containers =    '#disqus_thread, ' +
+                        'fb\\:comments, ' + // esc : for css selector
+                        '.comments_block_holder, ' + // huffpo
+                        '#ugccmt-container', // yahoo
                         i, j, t, elem, type;
 
     function main() {
         for (i=0; i<safeDomains.length; i++) {
             if (document.domain.indexOf(safeDomains[i]) == -1) {
-                for (i=0; i<containers.length; i++) {
-                    type = containers[i].charAt(0);
-                    if (type === '#') {
-                        elem = document.getElementById(containers[i].substr(1));
-                    } else if (type === '.') {
-                        elem = document.getElementsByClassName(containers[i].substr(1))[0];
-                    } else {
-                        elem = document.getElementsByTagName(containers[i])[0];
-                    }
-                    if (elem) {
-                        remove(elem);
+                elem = document.querySelectorAll(containers);
+                if (elem.length) {
+                    for (j=0; j<elem.length; j++) {
+                        remove(elem[j]);
                     }
                 }
             }
